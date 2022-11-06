1eb8fab814ebe246e17120c5332371f093f37f8e
maciejchwast@student.agh.edu.pl <maciejchwast@student.agh.edu.pl>
2022-11-06 16:28:41 +0100

    Automaty komorkowe 2D

--- main.py
+++ main.py
@@ -1,43 +1,113 @@
-from PIL import Image
+import pygame
+import numpy as np
+
+ALIVE_COLOR = (255, 255, 215)
+BACKGROUND_COLOR = (10, 10, 40)
+GRID_COLOR = (30, 30, 60)
+
+CELL_SIZE = 8
+
+SHOOTER = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 1, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 1, 1, 0, 0, 0],
+                    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0],
+                    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0, 0, 0]
+                    ])
+
+RANDOM = ""
+
+OSCILLATOR = np.array([[0, 0, 0, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [1, 1, 1, 0, 0, 0, 1, 1, 1],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 0, 0, 0, 0]
+                       ])
+
+CONSTANT = np.array([[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                     [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+                     [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+                     [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+                     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+                     ])
+
+GLIDERS = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                    [0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+                    [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                    [0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+                    [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0]
+                    ])
 
 
-def brighten(image):
-    height = image.height
-    width = image.width
-    result = image
+def update(surface, cur, sz):
+    nxt = np.zeros((cur.shape[0], cur.shape[1]))
 
-    value_brighten = input('Podaj wartosc z zakresu -255 - 255 o ktora obraz zostanie pojasniony lub sciemniony: ')
+    for r, c in np.ndindex(cur.shape):
+        num_alive = np.sum(cur[r - 1:r + 2, c - 1:c + 2]) - cur[r, c]
 
-    for x in range(width):
-        for y in range(height):
-            pixel = image.getpixel((x, y)) + int(value_brighten)
-            result.putpixel((x, y), pixel)
+        if cur[r, c] == 1 and num_alive < 2 or num_alive > 3:
+            col = ALIVE_COLOR
+        elif (cur[r, c] == 1 and 2 <= num_alive <= 3) or (cur[r, c] == 0 and num_alive == 3):
+            nxt[r, c] = 1
+            col = ALIVE_COLOR
 
-    result.save('output_brighten.bmp')
+        col = col if cur[r, c] == 1 else BACKGROUND_COLOR
+        pygame.draw.rect(surface, col, (c * sz, r * sz, sz - 1, sz - 1))
 
-
-def binarize(image):
-    height = image.height
-    width = image.width
-    result = image
-
-    value_binarize = input('Podaj wartosc graniczna binaryzacji: ')
-    for x in range(width):
-        for y in range(height):
-            pixel = image.getpixel((x, y))
-            if pixel < int(value_binarize):
-                result.putpixel((x, y), 0)
-            else:
-                result.putpixel((x, y), 255)
-
-    result.save('output_binarize.bmp')
+    return nxt
 
 
-if __name__ == '__main__':
-    path = "Mapa_MD_no_terrain_low_res_Gray.bmp"
-    image = Image.open(path)
-    brighten(image)
-    binarize(image)
+def init(dim_x, dim_y, pattern):
+    if pattern == RANDOM:
+        return np.random.randint(2, size=(dim_x, dim_y))
+    cells = np.zeros((dim_y, dim_x))
+    pos = (3, 3)
+    cells[pos[0]:pos[0] + pattern.shape[0], pos[1]:pos[1] + pattern.shape[1]] = pattern
+    return cells
 
 
+def main(dim_x, dim_y):
+    pygame.init()
+    surface = pygame.display.set_mode((dim_x * CELL_SIZE, dim_y * CELL_SIZE))
+    pygame.display.set_caption("Gra w życie")
+    cells = init(dim_x, dim_y, GLIDERS)  # zmiana trzeciego argumentu dla roznych struktur
+    surface.fill(GRID_COLOR)
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        surface.fill(GRID_COLOR)
+        cells = update(surface, cells, CELL_SIZE)
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main(120, 120)
